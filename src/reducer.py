60fbe929bf3c3d8845e30b307ada104c0c74c9ab
Julie7-liu <julie7.liu@gmail.com>
2017-12-01 12:56:10 +0800

    code for basic reducer

--- src/reducer.py
+++ src/reducer.py
@@ -1,1 +1,27 @@
-//reducer src
+#!/usr/bin/python
+
+import sys
+
+valTotal = 0
+prevKey = None
+
+for line in sys.stdin:
+    data = line.strip().split("\t")
+
+    if len(data) != 2:
+        print "Error in mapped line, ", line
+        continue
+
+    curKey, curVal = data
+
+    if prevKey and curKey != prevKey:
+        print "{0}\t{1}".format(prevKey, valTotal)
+        valTotal = 0
+    
+    valTotal += float(curVal)
+    prevKey = curKey
+
+if curKey:
+    print "{0}\t{1}".format(curKey, valTotal)
+        
+
